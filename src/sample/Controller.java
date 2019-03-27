148a209043c99330acbdb604497767ba2f4cb3dc
Vedad Bukva <vedadbukva8@gmail.com>
2019-03-27 21:21:24 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+    public TextField unos;
+
+    public TextField ispis;
+
+    public void akcija(ActionEvent actionEvent) {
+
+        String recenica = unos.getText();
+
+        String najduza = "";
+
+        String recenica2 = "";
+
+        for( String rijec : recenica.split(" ") ){
+            if(rijec.length() > najduza.length() )
+                najduza = rijec;
+        }
+
+
+        for (String rijec2 : recenica.split(" ")){
+            if ( rijec2.equals(najduza))
+                rijec2 = rijec2.toUpperCase();
+
+            recenica2 += rijec2 + " ";
+
+        }
+        ispis.setText(recenica2);
+
+
+    }
+}
